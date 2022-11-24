b75afd9e972ca6743cbec9ac920f49fe8b3813f5
camckenn <camckenn@umich.edu>
2022-11-24 13:24:03 -0500

    Read in csv and arranged/scaled dataset

--- LSTM/LSTM_airline_test.py
+++ LSTM/LSTM_airline_test.py
@@ -3,11 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
+from tensorflow import keras 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
 # fix random seed for reproducibility
 tf.random.set_seed(7)
+
+# load the dataset
+dataframe = pd.read_csv('airline-passengers.csv', usecols=[1], engine='python')
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
