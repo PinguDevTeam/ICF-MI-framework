dd345d482e59409710a9868572e452bdcbe9edff
camckenn <camckenn@umich.edu>
2022-11-23 20:20:59 -0500

    Beginning LSTM test from tutorial and uploading data csv

---
+++ LSTM/LSTM_airline_test.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+# fix random seed for reproducibility
+tf.random.set_seed(7)
