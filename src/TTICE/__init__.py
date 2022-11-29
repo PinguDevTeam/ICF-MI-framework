7177d1cc37201970f18025b23bcb16e2b4cf81a0
dorukaks <doruk@umich.edu>
2022-11-29 14:45:17 -0500

    (test) write a test function into TTICE for docs

---
+++ src/TTICE/__init__.py
@@ -0,0 +1,11 @@
+def testFcn(a):
+    """This is a test function to check if docstrings are working
+
+    Args:
+        a (float): random value
+
+    Returns:
+        (float): input plus 2
+    """
+    a += 2
+    return a
