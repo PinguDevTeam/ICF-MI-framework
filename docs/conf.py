772751e0d1e66a35e5a93adcacaa95eacbe90325
dorukaks <doruk@umich.edu>
2022-11-29 10:37:24 -0500

    (test) change rtd_theme import style

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,12 @@ release = "0.0.1"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-
 # configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
