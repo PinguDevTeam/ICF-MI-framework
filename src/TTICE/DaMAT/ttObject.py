637504f3fb7d722514a17fc3c8180729d092cd2c
dorukaks <doruk@umich.edu>
2022-12-13 11:45:54 -0500

    (feat) create ttObject and initialization function

--- src/TTICE/DaMAT/ttObject.py
+++ src/TTICE/DaMAT/ttObject.py
@@ -7,3 +7,120 @@ or incrementally using [TT-ICE algorithm](https://arxiv.org/abs/2211.12487).
 Furthermore, this object allows exporting and importing TT-cores using native
 format (`.ttc`) and intermediary formats (`.txt`).
 """
+
+import numpy as np
+
+
+class ttObject:
+    """
+    Python object for tensors in Tensor-Train format.
+
+    This object computes the TT-decomposition of multidimensional arrays using
+    `TTSVD`_, `TT-ICE`_, and `TT-ICE*`_.
+    It furthermore contains an inferior method, ITTD, for benchmarking purposes.
+
+    This object handles various operations in TT-format such as dot product and norm.
+    Also provides supporting operations for uncompressed multidimensional arrays such
+    as reshaping and transpose.
+    Once the tensor train approximation for a multidimensional array is computed, you
+    can compute projection of appropriate arrays onto the TT-cores, reconstruct
+    projected or compressed tensors and compute projection/compression accuracy.
+
+    You can also save/load tensors as `.ttc` or `.txt` files.
+    Attributes
+    ----------
+    inputType: type
+        Type of the input data. This determines how the object is initialized.
+    originalData:
+        Original multidimensional input array. Generally will not be stored
+        after computing
+        an initial set of TT-cores.
+    method: :obj:`str`
+        Method of computing the initial set of TT-cores. Currently only accepts
+        `'ttsvd'` as input
+    ttEpsilon: :obj:`float`
+        Desired relative error upper bound.
+    ttCores: :obj:`list` of :obj:`numpy.array`
+        Cores of the TT-decomposition. Stored as a list of numpy arrays.
+    ttRanks: :obj:`list` of :obj:`int`
+        TT-ranks of the decomposition. Stored as a list of integers.
+    nCores: :obj:`int`
+        Number of TT-cores in the decomposition.
+    nElements: :obj:`int`
+        Number of entries present in the current `ttObject`.
+    originalShape: :obj:`tuple` or :obj:`list`
+        Original shape of the multidimensional array.
+    reshapedShape: :obj:`tuple` or :obj:`list`
+        Shape of the multidimensional array after reshaping. Note that
+        this attribute is only meaningful before computing a TT-decomposition
+    indexOrder: :obj:`list` of :obj:`int`
+        Keeps track of original indices in case of transposing the input array.
+    .. _TTSVD:
+        https://epubs.siam.org/doi/epdf/10.1137/090752286
+        _TT-ICE:
+        https://arxiv.org/abs/2211.12487
+        _TT-ICE*:
+        https://arxiv.org/abs/2211.12487
+
+    """
+
+    def __init__(
+        self,
+        data,
+        epsilon: float = None,
+        keepData: bool = False,
+        samplesAlongLastDimension: bool = True,
+        method: str = "ttsvd",
+    ) -> None:
+        """
+        Initializes the ttObject.
+
+        Parameters
+        ----------
+        data: :obj:`numpy.array` or :obj:`list`
+            Main input to the ttObject. It can either be a multidimensional
+            `numpy array` or `list of numpy arrays`.
+            If list of numpy arrays are presented as input, the object will interpret it
+            as the TT-cores of an existing decomposition.
+        epsilon: :obj:`float`, optional
+            The relative error upper bound desired for approximation. Optional for cases
+            when `data` has type `list`.
+        keepData: :obj:`bool`, optional
+            Optional boolean variable to determine if the original array will be kept
+            after compression. Set to `False` by default.
+        samplesAlongLastDimension: :obj:`bool`, optional
+            Boolean variable to ensure if the samples are stacked along the last
+            dimension. Assumed to be `True` since it is one of the assumptions in
+            TT-ICE.
+        method: :obj:`str`, optional
+            Determines the computation method for tensor train decomposition of
+            the multidimensional array presented as `data`. Set to `'ttsvd'` by default.
+
+            Currently the package only has support for ttsvd, additional support such as
+            `ttcross` might be included in the future.
+
+        """
+        # self.ttRanks=ranks
+        self.ttCores = None
+        self.nCores = None
+        self.nElements = None
+        self.inputType = type(data)
+        self.method = method
+        self.keepOriginal = keepData
+        self.originalData = data
+        self.samplesAlongLastDimension = samplesAlongLastDimension
+        if self.inputType is np.memmap:
+            self.inputType = np.ndarray
+
+        if self.inputType == np.ndarray:
+            self.ttEpsilon = epsilon
+            self.originalShape = list(data.shape)
+            self.reshapedShape = self.originalShape
+            self.indexOrder = [idx for idx in range(len(self.originalShape))]
+        elif self.inputType == list:
+            self.nCores = len(data)
+            self.ttCores = data
+            self.reshapedShape = [core.shape[1] for core in self.ttCores]
+            self.updateRanks()
+        else:
+            raise TypeError("Unknown input type!")
