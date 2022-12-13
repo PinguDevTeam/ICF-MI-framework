005b1585cce6ac3f5b41bcd20aa9b901829503fd
dorukaks <doruk@umich.edu>
2022-12-13 12:42:51 -0500

    (fix) check docstrings

--- src/TTICE/ttObject.py
+++ src/TTICE/ttObject.py
@@ -1,5 +1,4 @@
-"""
-This is a python class for tensors in TT-format.
+"""This is a python class for tensors in TT-format.
 Through this object you can compute TT-decomposition of multidimensional arrays in
 one shot using [TTSVD algorithm](https://epubs.siam.org/doi/epdf/10.1137/090752286)
 or incrementally using [TT-ICE algorithm](https://arxiv.org/abs/2211.12487).
